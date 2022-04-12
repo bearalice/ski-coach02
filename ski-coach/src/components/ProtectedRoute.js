a61953db862a3868a4137ddfafa180ddeb91652c
bearalice <alicesshen@gmail.com>
2022-04-11 17:36:21 -0700

    deleted files

--- ski-coach/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { withAuthenticationRequired } from '@auth0/auth0-react'
-
-export default function ProtectedRoute({ protectedCompo }) {
-    const Compo = withAuthenticationRequired(protectedCompo);
-    return (
-        <Compo />
-    )
-}
