c37a962075b45bdf4a57f17fa85fb4fa1065c083
Alice S <alices@Alices-MacBook-Pro.local>
2022-04-12 21:05:09 -0700

    fixed navbar

--- ski-coach/src/components/NavBar.js
+++ ski-coach/src/components/NavBar.js
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import LogoutButton from './LogoutButton'
 import LoginButton from './LoginButton'
+import { Link } from 'react-router-dom'
 
 export default function NavBar({ isAuthed }) {
     return (
@@ -12,12 +13,12 @@ export default function NavBar({ isAuthed }) {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav>
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/coaches">Coaches</Nav.Link>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/coaches">Coaches</Nav.Link>
                             {isAuthed ?
-                                <Nav.Link href="/profile">My Profile</Nav.Link>
-                                : <Nav.Link href="/profile">Profile</Nav.Link>}
-                            {isAuthed ? <Nav.Link href="/new">Add Coach</Nav.Link> : null}
+                                <Nav.Link as={Link} to="/profile">My Profile</Nav.Link>
+                                : <Nav.Link as={Link} to="/profile">Profile</Nav.Link>}
+                            {isAuthed ? <Nav.Link as={Link} to="/new">Add Coach</Nav.Link> : null}
                             {isAuthed ? <LogoutButton /> : <LoginButton />}
 
                         </Nav>
